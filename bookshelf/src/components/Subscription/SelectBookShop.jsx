90f746327d2044dff8718a531e4c1454784d3866
hasindu karunathilaka <hasindukarunathilaka343@gmail.com>
2023-08-01 18:19:48 +0530

    edit

--- bookshelf/src/components/Subscription/SelectBookShop.jsx
+++ bookshelf/src/components/Subscription/SelectBookShop.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Flex, Grid, GridItem } from '@chakra-ui/react'
+import { Box, Center, Flex, Grid, GridItem } from '@chakra-ui/react'
 import Search from '../../components/Shop/Search'
 import Filter from '../../components/Shop/Filter'
 import CategoryFilter from '../../components/Shop/CategoryFilter'
@@ -83,19 +83,17 @@ function SelectBookShop() {
                 <GridItem rowSpan={1} colSpan={5} p={2}>
                     <Filter />
                 </GridItem>
-                <GridItem rowSpan={8} colSpan={1} border={'1px'} borderColor={'blue.200'} rounded={'md'}>
+                <GridItem rowSpan={8} colSpan={1} border={'1px'}  borderColor={'blue.200'} rounded={'md'}>
                     <CategoryFilter />
                 </GridItem>
-                <GridItem rowSpan={8} colSpan={4} border={'1px'} borderColor={'blue.200'} rounded={'md'}>
-                    <Flex flexWrap={'wrap'} gap={10} p={10}>
+                <GridItem rowSpan={8} colSpan={4} border={'1px'} borderColor={'blue.200'} rounded={'md'} >
+                    <Flex justifyContent={'center'} flexWrap={'wrap'} gap={10} p={10}>
                         {Object.keys(bookDetails).map((item) => (
                             <BookCardSelecBooks
                                 key={item}
                                 name={bookDetails[item].title}
-                                author={bookDetails[item].author}
                                 price={bookDetails[item].price}
                                 imageURL={bookDetails[item].image}
-                                rating={bookDetails[item].rating}
                             />
                         ))}
                     </Flex>
