fcb8652c36e1126acc3dd4208501e540f289efae
AnushkaD99 <92775945+AnushkaD99@users.noreply.github.com>
2023-08-05 01:26:28 +0530

    update changes

--- bookshelf/src/App.jsx
+++ bookshelf/src/App.jsx
@@ -15,6 +15,18 @@ import { useState, useMemo } from "react";
 
 import {UserProvider} from './context/userContext';
 
+// Import Moderator Pages
+import Dashboard from "./pages/Moderator/Dashboard";
+import Notifications from "./pages/Moderator/Notifications";
+import Inventry from "./pages/Moderator/Inventry";
+import Shop_M from "./pages/Moderator/Shop_M";
+import Orders from "./pages/Moderator/Orders";
+import Donations from "./pages/Moderator/Donations";
+import Subscriptions from "./pages/Moderator/Subscriptions";
+import BookSharing from "./pages/Moderator/BookSharing";
+import Setting from "./pages/Moderator/Settings";
+import AdddNewBook from "./pages/Moderator/AdddNewBook";
+
 function App() {
 
   return (
@@ -58,83 +70,12 @@ function App() {
           <Route exact path="/shop/:id" element={<ProductPage />}></Route>
           <Route exact path="/cart/:userId" element={<Cart />}></Route>
           {/* <Route exact path="/logout" element={<Logout />}></Route> */}
-        </Routes>
-        <Footer />
-        </UserProvider>
-      </Router>
-
-      
-    </Box>
-  );
-}
-
-export default App;
-
-import { Box, Container, useColorModeValue, Image } from "@chakra-ui/react";
-import Navbar from "./components/Navbar";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import g from "./assets/g.png";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-
-// Import Moderator Pages
-import Dashboard from "./pages/Moderator/Dashboard";
-import Notifications from "./pages/Moderator/Notifications";
-import Inventry from "./pages/Moderator/Inventry";
-import Shop from "./pages/Moderator/Shop";
-import Orders from "./pages/Moderator/Orders";
-import Donations from "./pages/Moderator/Donations";
-import Subscriptions from "./pages/Moderator/Subscriptions";
-import BookSharing from "./pages/Moderator/BookSharing";
-import Setting from "./pages/Moderator/Settings";
-import AdddNewBook from "./pages/Moderator/AdddNewBook";
-
-function App() {
-  return (
-    
-    <Box
-      
-      h={"200vh"}
-     
-      // objectFit={"cover"}
-      
-      // backgroundImage={''}
-      // backgroundAttachment={"fixed"}
-      // backgroundSize={"90% auto"}
-      // backgroundRepeat={"no-repeat"}
-      // backgroundPosition={"right"}
-      // backgroundOpacity={"30%"}
-    >
-   
-    {/* backGround 3D */}
-      {/* <Image
-        boxSize={"100vh"}
-        objectFit={"cover"}
-        src={g}
-    
-        position="absolute"
-        top={"-140px"}
-        left={"40vw"}
-        right={0}
-        bottom={0}
-        opacity={"90%"}
-
-
-      /> */}
-    
-      <Router>
-      <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Home />}></Route>
-          <Route exact path="/login" element={<Login />}></Route>
-          <Route exact path="/register" element={<Register />}></Route>
 
           {/* Moderator Routes */}
           <Route exact path="/moderator/dashboard" element={<Dashboard />}></Route>
           <Route exact path="/moderator/notifications" element={<Notifications />}></Route>
           <Route exact path="/moderator/inventry" element={<Inventry />}></Route>
-          <Route exact path="/moderator/shop" element={<Shop />}></Route>
+          <Route exact path="/moderator/shop_M" element={<Shop_M />}></Route>
           <Route exact path="/moderator/orders" element={<Orders />}></Route>
           <Route exact path="/moderator/donations" element={<Donations />}></Route>
           <Route exact path="/moderator/subscriptions" element={<Subscriptions />}></Route>
@@ -142,7 +83,11 @@ function App() {
           <Route exact path="/moderator/setting" element={<Setting />}></Route>
           <Route exact path="/moderator/addNewBook" element={<AdddNewBook />}></Route>
         </Routes>
+        <Footer />
+        </UserProvider>
       </Router>
+
+      
     </Box>
   );
 }
--- bookshelf/src/pages/Moderator/Shop_M.jsx
+++ bookshelf/src/pages/Moderator/Shop_M.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Shop() {
+export default function Shop_M() {
   return (
     <div>Shop</div>
   )
