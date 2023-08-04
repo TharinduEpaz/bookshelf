00bdc3d1feffcbd84386117f48acefe9934f6af8
AnushkaD99 <92775945+AnushkaD99@users.noreply.github.com>
2023-08-05 00:01:14 +0530

    Revert "Revert "revert merge""
    This reverts commit 62f29ffc378650ca97672f255411e59327399d33.

--- bookshelf/src/pages/Home.jsx
+++ bookshelf/src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import BlurWhiteBox from "../components/Home/BlurWhiteBox";
 import HomeItemBox from "../components/Home/HomeItemBox";
-import { Box, Center, Divider, Flex, Heading } from "@chakra-ui/react";
+import { Center, Divider, Flex, Heading} from "@chakra-ui/react";
 import bookgirl from "../assets/bookgirl.png";
 import { FaBookOpen, FaFire, FaHeart } from "react-icons/fa";
 import { BsStars } from "react-icons/bs";
 import { ImBooks } from "react-icons/im";
-import BookCard from "../components/Home/BookCard";
-import Features from "../components/Home/Features";
+
+
 
 function Home() {
   const itemBoxDetails = {
@@ -37,133 +37,58 @@ function Home() {
       icon: <ImBooks size={25} color="#FF9798" />,
     },
   };
-
-  const newArrivals = {
-    book1: {
-      title: "The Midnight Library",
-      author: "Matt Haig",
-      image: "https://images-na.ssl-images-amazon.com/images/I/81h2gWPTYJL.jpg",
-      price: "14.99",
-      rating: 4,
-    },
-    book2: {
-      title: "The Vanishing Half",
-      author: "Brit Bennett",
-      image: "https://m.media-amazon.com/images/I/81ICvbFe2+L.jpg",
-      price: "14.99",
-      rating: 4.5,
-    },
-    book3: {
-      title: "The Four Winds",
-      author: "Kristin Hannah",
-      image: "https://m.media-amazon.com/images/I/6132R6AHGjL.jpg",
-      price: "14.99",
-      rating: 3.2,
-    },
-    book4: {
-      title: "The Sanatorium",
-      author: "Sarah Pearse",
-      image: "https://m.media-amazon.com/images/I/51k-rWw95NL.jpg",
-      price: "14.99",
-      rating: 4,
-    },
-    book5: {
-      title: "The Push",
-      author: "Ashley Audrain",
-      image: "https://m.media-amazon.com/images/I/41ClAKnvFqL.jpg",
-      price: "14.99",
-      rating: 4.9,
-    },
-    book6: {
-      title: "The Survivors",
-      author: "Jane Harper",
-      image: "https://m.media-amazon.com/images/I/51Q1qQ9YJTL.jpg",
-      price: "14.99",
-      rating: 4.5,
-    },
-
-  };
-
   return (
     <>
       <BlurWhiteBox
         title="Find Your Next Literary Escape Browse, Click, and Get Lost in Stories!"
         image={bookgirl}
-        mainButton="Shop"
-        route="/shop"
       />
-      <Box
-        m={"auto"}
-        mt={10}
-        w="80%"
-        borderRadius="2xl"
-        boxShadow="sm"
-        bgGradient="linear(to left, rgba(255, 255, 235, 0.2), rgba(255, 255, 255, 0.2))"
-        // filter="blur(8px)"
-        backdropFilter="blur(8px)"
-        p={10}
+      <Center>
+      <Heading
+        alignSelf={"center"}
+        
+        fontWeight={'light'}
+        size={"sm"}
+        mt={20}
+        letterSpacing={5}
+        
       >
-        <Center>
-          <Heading
-            alignSelf={"center"}
-            fontWeight={"light"}
-            size={"sm"}
-            fontFamily={"Montserrat"}
-          >
-            Featured Categories
-          </Heading>
-        </Center>
+      
+        FEATURED CATEGORIES
+      </Heading>
+      
+      </Center>
 
-        <Flex
-          gap={10}
-          alignItems={"center"}
-          justifyContent={"center"}
-          w={"100%"}
-          mt={10}
-          flexWrap={"wrap"}
-          
-        >
-          {Object.keys(itemBoxDetails).map((item) => (
-            <HomeItemBox
-              key={item}
-              header={itemBoxDetails[item].header}
-              bgColor={itemBoxDetails[item].bgColor}
-              icon={itemBoxDetails[item].icon}
-            />
-          ))}
-        </Flex>
-        <Center>
-          <Heading
-            alignSelf={"center"}
-            fontWeight={"light"}
-            size={"sm"}
-            fontFamily={"Montserrat"}
-            mt={20}
-          >
-            New Arrivals
-          </Heading>
-        </Center>
-        <Flex
-          gap={10}
-          alignItems={"center"}
-          justifyContent={"center"}
-          w={"100%"}
-          mt={10}
-          flexWrap={"wrap"}
-        >
-          {Object.keys(newArrivals).map((item) => (
-            <BookCard
-              key={item}
-              name={newArrivals[item].title}
-              author={newArrivals[item].author}
-              price={newArrivals[item].price}
-              imageURL={newArrivals[item].image}
-              rating={newArrivals[item].rating}
-            />
-          ))}
-        </Flex>
-        <Features />
-      </Box>
+      <Flex
+        gap={10}
+        alignItems={"center"}
+        justifyContent={"center"}
+        w={"100%"}
+        mt={20}
+        flexWrap={"wrap"}
+      >
+        {Object.keys(itemBoxDetails).map((item) => (
+          <HomeItemBox
+            key={item}
+            header={itemBoxDetails[item].header}
+            bgColor={itemBoxDetails[item].bgColor}
+            icon={itemBoxDetails[item].icon}
+          />
+        ))}
+      </Flex>
+      <Center>
+      <Heading
+        alignSelf={"center"}
+        
+        fontWeight={'light'}
+        size={"sm"}
+        mt={20}
+        letterSpacing={5}
+        
+      >
+        NEW ARRIVALS
+      </Heading>
+      </Center>
     </>
   );
 }
--- bookshelf/src/pages/Register.jsx
+++ bookshelf/src/pages/Register.jsx
@@ -13,91 +13,14 @@ import {
   Text,
   useColorModeValue,
   Link,
-  Alert,  
-  AlertIcon,
-  SkeletonCircle,
-  SkeletonText,
-  Spinner,
-  CircularProgress,
-
-  
-
-
-
 } from '@chakra-ui/react';
 
 import { useState } from 'react';
 //import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
 import { Fa500Px } from 'react-icons/fa';
-import axios from 'axios';
 
 export default function SignupCard() {
   const [showPassword, setShowPassword] = useState(false);
-  const[firstName,setFirstName] = useState('');
-  const[lastName,setLastName] = useState('');
-  const [email,setEmail] = useState('');
-  const [password,setPassword] = useState('');
-  const [error,setError] = useState('');
-  const [isLoading,setIsLoading] = useState(false);
-
-  const regUrl = 'http://localhost:3000/api/v1/register';
-
-  const register = async (e) => {
-     e.preventDefault();
-     try {
-      setIsLoading(true);
-    
-      const response = await axios.post(regUrl,{ firstName : firstName, lastName : lastName, email : email, password : password});
-      console.log(response.data);
-      setEmail('');
-      setPassword('');
-      setFirstName('');
-      setLastName('');
-      setIsLoading(false);
-
-      window.location.href = "/login";
-      
-     } catch (error) {
-      setError(error.response.data.msg);  
-      setIsLoading(false);
-    console.log(error.response);
-     }
-  
-  }
-
-  if(isLoading){
-    return (
-      <Flex
-      minH={'100vh'}
-      
-      justify={'center'}
-      >
-      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
-        <Stack align={'center'}>
-          <Heading fontSize={'4xl'} textAlign={'center'}>
-            Sign up
-          </Heading>
-          <Text fontSize={'lg'} color={'gray.600'}>
-            to enjoy all of our cool features ✌️
-          </Text>
-        </Stack>
-        <Box
-          rounded={'lg'}
-          bg={useColorModeValue('white', 'gray.700')}
-          boxShadow={'lg'}
-          p={8}>
-          <Flex alignItems={'center'} justifyContent={'center'}>
-          <CircularProgress isIndeterminate color='green.300' />
-            
-          </Flex>
-        </Box>
-      </Stack>
-    </Flex>
-    )
-  }
-
-
-
 
   return (
     <Flex
@@ -120,43 +43,28 @@ export default function SignupCard() {
           boxShadow={'lg'}
           p={8}>
           <Stack spacing={4}>
-          {error && <Alert status="error"> <AlertIcon /> {error}</Alert>} 
-          <form onSubmit={register}>
             <HStack>
               <Box>
                 <FormControl id="firstName" isRequired>
                   <FormLabel>First Name</FormLabel>
-                  <Input type="text" 
-                  value={firstName}
-                  onChange={(e) => setFirstName(e.target.value)}
-    
-                  />
+                  <Input type="text" />
                 </FormControl>
               </Box>
               <Box>
                 <FormControl id="lastName">
                   <FormLabel>Last Name</FormLabel>
-                  <Input type="text" 
-                  value={lastName}
-                  onChange={(e) => setLastName(e.target.value)}
-                  />
+                  <Input type="text" />
                 </FormControl>
               </Box>
             </HStack>
             <FormControl id="email" isRequired>
               <FormLabel>Email address</FormLabel>
-              <Input type="email" 
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              />
+              <Input type="email" />
             </FormControl>
             <FormControl id="password" isRequired>
               <FormLabel>Password</FormLabel>
               <InputGroup>
-                <Input type={showPassword ? 'text' : 'password'} 
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                />
+                <Input type={showPassword ? 'text' : 'password'} />
                 <InputRightElement h={'full'}>
                   <Button
                     variant={'ghost'}
@@ -170,7 +78,6 @@ export default function SignupCard() {
             </FormControl>
             <Stack spacing={10} pt={2}>
               <Button
-              type='submit'
                 loadingText="Submitting"
                 size="lg"
                 bg={'blue.400'}
@@ -186,7 +93,6 @@ export default function SignupCard() {
                 Already a user? <Link color={'blue.400'}>Login</Link>
               </Text>
             </Stack>
-            </form>
           </Stack>
         </Box>
       </Stack>
