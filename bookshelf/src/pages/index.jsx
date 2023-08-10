8059f95a82c8aa9b1c6eb8c4803a88982a1140fb
hasindu karunathilaka <hasindukarunathilaka343@gmail.com>
2023-08-10 21:03:03 +0530

    edit index,js

--- bookshelf/src/pages/index.jsx
+++ bookshelf/src/pages/index.jsx
@@ -5,7 +5,6 @@ import Login from "./Login";
 import ProductPage from "./ProductPage";
 import Register from "./Register";
 import Shop from "./Shop";
-import Register2 from "./Register2";
 
 
-export {Account, Cart, Home, Login, ProductPage, Register, Shop, Register2};
+export {Account, Cart, Home, Login, ProductPage, Register, Shop};
