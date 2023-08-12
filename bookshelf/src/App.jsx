748f02496c6c3ec1d4e2f495b33f730609006a49
Tharindu Epasingha <2020cs050@stu.ucsc.cmb.ac.lk>
2023-08-12 11:09:47 +0530

    added protected routes

--- bookshelf/src/App.jsx
+++ bookshelf/src/App.jsx
@@ -1,51 +1,63 @@
-import { Box, Container, useColorModeValue, Image,Icon,useBreakpointValue } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  useColorModeValue,
+  Image,
+  Icon,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 import g from "./assets/g.png";
 
+import { UserProvider } from "./context/userContext";
 
+import {
+  Account,
+  Cart,
+  Home,
+  Login,
+  ProductPage,
+  Register,
+  Shop,
+} from "./pages";
 
-import {UserProvider} from './context/userContext';
+import { Dashboard, Settings, Orders, Chat } from "./components/Account";
 
-import {Account, Cart, Home, Login, ProductPage, Register, Shop} from "./pages";
-
-import {Dashboard, Settings, Orders, Chat} from "./components/Account";
+import { PrivateRoutes } from "./utils/privateRoutes";
 
 const Blur = (props) => {
   return (
     <Icon
-      width={useBreakpointValue({ base: '100%', md: '40vw', lg: '30vw' })}
+      width={useBreakpointValue({ base: "100%", md: "40vw", lg: "30vw" })}
       zIndex={useBreakpointValue({ base: -1, md: -1, lg: 0 })}
       height="560px"
       viewBox="0 0 528 560"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      {...props}>
-    <circle cx="100%" cy="100%" r="100%" fill="hsla(1334, 86%, 52%, 0.2)" />
+      {...props}
+    >
+      <circle cx="100%" cy="100%" r="100%" fill="hsla(1334, 86%, 52%, 0.2)" />
       {/* <circle cx="100%" cy="100%" r="100%" fill="hsla(343, 91%, 58%, 1)" /> */}
       <circle cx="85%" cy="43%" r="50%" fill="hsla(194, 89%, 52%, 0.2)" />
     </Icon>
-  )
-}
+  );
+};
 
 function App() {
-
   return (
-    
     <Box
-      // objectFit={"cover"}
-      // backgroundImage={''}
-      // backgroundAttachment={"fixed"}
-      // backgroundSize={"90% auto"}
-      // backgroundRepeat={"no-repeat"}
-      // backgroundPosition={"right"}
-      // backgroundOpacity={"30%"}
-      
+    // objectFit={"cover"}
+    // backgroundImage={''}
+    // backgroundAttachment={"fixed"}
+    // backgroundSize={"90% auto"}
+    // backgroundRepeat={"no-repeat"}
+    // backgroundPosition={"right"}
+    // backgroundOpacity={"30%"}
     >
-   
-    {/* backGround 3D */}
+      {/* backGround 3D */}
       {/* <Image
         boxSize={"100vh"}
         objectFit={"cover"}
@@ -60,34 +72,31 @@ function App() {
 
 
       /> */}
-     
-    
-      <Router>
-      <UserProvider>
-      <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Home />}></Route>
-          <Route exact path="/login" element={<Login />}></Route>
-          <Route exact path="/register" element={<Register />}></Route>
-          <Route exact path="/shop" element={<Shop />}></Route>
-          <Route exact path="/shop/:id" element={<ProductPage />}></Route>
-          <Route exact path="/cart/:userId" element={<Cart />}></Route>
 
-          <Route exact path="/account" element={<Account />}>
-            <Route index element={<Dashboard />} />
-            <Route path="dashboard" element={<Dashboard />} />
-            <Route path = "settings" element={<Settings />} />
-            <Route path = "orders" element={<Orders />} />
-            <Route path = "chat" element={<Chat />} />
-          </Route>
-          
-        </Routes>
-        <Footer />
+      <Router>
+        <UserProvider>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route exact path="/login" element={<Login />}></Route>
+            <Route exact path="/register" element={<Register />}></Route>
+            <Route exact path="/shop" element={<Shop />}></Route>
+            <Route exact path="/shop/:id" element={<ProductPage />}></Route>
+            <Route exact path="/cart/:userId" element={<Cart />}></Route>
+
+            <Route element={<PrivateRoutes />}>
+              <Route exact path="/account" element={<Account />}>
+                <Route index element={<Dashboard />} />
+                <Route path="dashboard" element={<Dashboard />} />
+                <Route path="settings" element={<Settings />} />
+                <Route path="orders" element={<Orders />} />
+                <Route path="chat" element={<Chat />} />
+              </Route>
+            </Route>
+          </Routes>
+          <Footer />
         </UserProvider>
       </Router>
-      
-
-      
     </Box>
   );
 }
