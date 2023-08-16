b6b869e32ceb44643bcd65708befb7d34e23def1
AnushkaD99 <92775945+AnushkaD99@users.noreply.github.com>
2023-08-16 20:56:09 +0530

    notification improved

--- bookshelf/src/components/Moderator/NotificationCard.jsx
+++ bookshelf/src/components/Moderator/NotificationCard.jsx
@@ -1,13 +1,16 @@
-import { Card, CardBody, Flex, Spacer, Text } from "@chakra-ui/react";
+import { Card, CardBody, Flex, Spacer, Stack, Text } from "@chakra-ui/react";
 import React from "react";
 
-export default function NotificationCard({ text, time }) {
+export default function NotificationCard({ text, time, description }) {
   return (
     <>
       <Card>
         <CardBody>
           <Flex>
-            <Text fontWeight={"regular"}>{text}</Text>
+            <Stack>
+            <Text fontWeight={"semibold"}>{text}</Text>
+            <Text>{description}</Text>
+            </Stack>
             <Spacer />
             <Text fontSize="xs" color="gray.500" mt={5}>
               {time}
--- bookshelf/src/pages/Moderator/M_Notifications.jsx
+++ bookshelf/src/pages/Moderator/M_Notifications.jsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Box, Flex, Spacer, Text } from "@chakra-ui/react";
 import React from "react";
 import NotificationCard from "../../components/Moderator/NotificationCard";
 
@@ -14,19 +14,16 @@ export default function Notifications() {
 
         <Flex gap={10} direction={"column"} mt={10}>
           <Flex gap={2} direction={"column"}>
-            <Text fontSize={"sm"} fontWeight={"semibold"}>
+            <Text fontSize={"sm"} fontWeight={"bold"}>
               Today
             </Text>
-            <NotificationCard text="New user registered" time="9.52 P.M" />
-            <NotificationCard text="New order received" time="8.00 P.M" />
-          </Flex>
-
-          <Flex gap={2} direction={"column"}>
-            <Text fontSize={"sm"} fontWeight={"semibold"}>
-              Yesterday
-            </Text>
-            <NotificationCard text="New user registered" time="9.52 P.M" />
-            <NotificationCard text="New user registered" time="8.00 P.M" />
+            <NotificationCard text="New user registered" time="9.52 P.M" description={<>Name : Ravindu Viranga  <Spacer/>  Email: ravindu@gmail.com </>} />
+            <NotificationCard text="New order" time="8.00 P.M" description={<>Book Name : Mystery at Midnight Manor <Spacer /> Quantity : 1</>} />
+            <NotificationCard
+              text="New donation request"
+              time="7.00 P.M"
+              description="A new donation request has been submitted by a user."
+            />
           </Flex>
         </Flex>
       </Box>
