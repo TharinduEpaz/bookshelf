b8cfe3affcc70951afa0d1d356ded0e7221fa77f
hasindu karunathilaka <hasindukarunathilaka343@gmail.com>
2023-08-17 09:07:43 +0530

    edit

--- bookshelf/src/components/Account/chat.jsx
+++ bookshelf/src/components/Account/chat.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+// import React from 'react'
 
-function chat() {
+// function chat() {
+//   return (
+//     <div>
+
+//     </div>
+//   )
+// }
+
+// export default chat
+import { Flex } from "@chakra-ui/react";
+import React, { useState } from "react";
+// import Divider from "../components/Divider";
+// import Footer from "../components/Footer";
+// import Header from "../components/Header";
+// import Messages from "../components/Messages";
+
+const Chat = () => {
+  const [messages, setMessages] = useState([
+    { from: "computer", text: "Hi, My Name is HoneyChat" },
+    { from: "me", text: "Hey there" },
+    { from: "me", text: "Myself Ferin Patel" },
+    {
+      from: "computer",
+      text: "Nice to meet you. You can send me message and i'll reply you with same message.",
+    },
+  ]);
+  const [inputMessage, setInputMessage] = useState("");
+
+  const handleSendMessage = () => {
+    if (!inputMessage.trim().length) {
+      return;
+    }
+    const data = inputMessage;
+
+    setMessages((old) => [...old, { from: "me", text: data }]);
+    setInputMessage("");
+
+    setTimeout(() => {
+      setMessages((old) => [...old, { from: "computer", text: data }]);
+    }, 1000);
+  };
+
   return (
-    <div>chat</div>
-  )
-}
+    <>
+    
+    </>
+  );
+};
 
-export default chat
+export default Chat;
