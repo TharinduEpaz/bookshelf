72a1d30dc36d7ea64e25d25bc5ec039e2c3caf10
Tharindu Epasingha <2020cs050@stu.ucsc.cmb.ac.lk>
2023-08-17 09:56:55 +0530

    chat added

--- bookshelf/src/components/Account/chat.jsx
+++ bookshelf/src/components/Account/chat.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useState } from "react";
 
-function chat() {
+const chatContainerStyle = {
+  width: "300px",
+  height: "400px",
+  border: "1px solid #ccc",
+  overflow: "hidden",
+  position: "relative",
+};
+
+const messageStyle = {
+  marginBottom: "10px",
+  padding: "8px",
+  borderRadius: "8px",
+  maxWidth: "80%",
+};
+
+const userMessageStyle = {
+  ...messageStyle,
+  backgroundColor: "#cceeff",
+  alignSelf: "flex-end",
+};
+
+const botMessageStyle = {
+  ...messageStyle,
+  backgroundColor: "#f0f0f0",
+  alignSelf: "flex-start",
+};
+
+const ChatMessage = ({ message, isUser }) => (
+  <div style={isUser ? userMessageStyle : botMessageStyle}>
+    {message.text}
+  </div>
+);
+
+const ChatInterface = () => {
+  const [messages, setMessages] = useState([
+    { text: "Hello! How can I help you?", isUser: false },
+    { text: "Hi there! I have a question.", isUser: true },
+    { text: "Sure, feel free to ask.", isUser: false },
+  ]);
+
   return (
-    <div>chat</div>
-  )
-}
+    <div style={{display:'flex',width:'100%',justifyContent:'center'}}>
+    <div style={chatContainerStyle} >
+      <div style={{ padding: "10px", overflowY: "auto", height: "calc(100% - 40px)" }}>
+        {messages.map((message, index) => (
+          <ChatMessage
+            key={index}
+            message={message}
+            isUser={message.isUser}
+          />
+        ))}
+      </div>
+    </div>
+    </div>
+  );
+};
 
-export default chat
+export default ChatInterface;
