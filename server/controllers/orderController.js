cfd2928fbbf857317b9b055a76266049dfc7a180
AnushkaD99 <92775945+AnushkaD99@users.noreply.github.com>
2023-08-19 21:06:51 +0530

    fixed order controller error

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -31,6 +31,7 @@ const createOrder = async (req, res, next) => {
             totalPrice,
             buyer_id,
             orderDate,
+            orderStatus,
         });
         res.status(statusCodes.StatusCodes.CREATED).json(order);
     } catch (err) {
