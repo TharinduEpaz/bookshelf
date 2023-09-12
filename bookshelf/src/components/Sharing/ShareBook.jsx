eea9a44a480c1ae5a816207ed3736b90d771c8e6
lasindu wathsan <lasinduwathsan@gmail.com>
2023-09-13 01:18:09 +0530

    sharebook

--- bookshelf/src/components/Sharing/ShareBook.jsx
+++ bookshelf/src/components/Sharing/ShareBook.jsx
@@ -30,32 +30,32 @@ function ShareBook() {
     };
     getRequestDetails();
   }, []);
-  const bookDetails = {
-    book1: {
-      id: '1',
-      title: "The Midnight Library",
-      author: "Matt Haig",
-      image: "https://images-na.ssl-images-amazon.com/images/I/81h2gWPTYJL.jpg",
-    },
-    book2: {
-      id: '2',
-      title: "The Vanishing Half",
-      author: "Brit Bennett",
-      image: "https://m.media-amazon.com/images/I/81ICvbFe2+L.jpg",
-    },
-    book3: {
-      id: '3',
-      title: "The Four Winds",
-      author: "Kristin Hannah",
-      image: "https://m.media-amazon.com/images/I/6132R6AHGjL.jpg",
-    },
-    book4: {
-      id: '4',
-      title: "The Sanatorium",
-      author: "Sarah Pearse",
-      image: "https://m.media-amazon.com/images/I/51k-rWw95NL.jpg",
-    },
-  }
+  // const bookDetails = {
+  //   book1: {
+  //     id: '1',
+  //     title: "The Midnight Library",
+  //     author: "Matt Haig",
+  //     image: "https://images-na.ssl-images-amazon.com/images/I/81h2gWPTYJL.jpg",
+  //   },
+  //   book2: {
+  //     id: '2',
+  //     title: "The Vanishing Half",
+  //     author: "Brit Bennett",
+  //     image: "https://m.media-amazon.com/images/I/81ICvbFe2+L.jpg",
+  //   },
+  //   book3: {
+  //     id: '3',
+  //     title: "The Four Winds",
+  //     author: "Kristin Hannah",
+  //     image: "https://m.media-amazon.com/images/I/6132R6AHGjL.jpg",
+  //   },
+  //   book4: {
+  //     id: '4',
+  //     title: "The Sanatorium",
+  //     author: "Sarah Pearse",
+  //     image: "https://m.media-amazon.com/images/I/51k-rWw95NL.jpg",
+  //   },
+  // }
   return (
     <Box
       border={"1px"}
