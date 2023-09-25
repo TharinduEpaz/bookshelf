d859b8ee0655ff5d96b7dffec59746fc24b729ea
lasindu wathsan <lasinduwathsan@gmail.com>
2023-09-25 15:25:16 +0530

    gg

--- bookshelf/src/components/Sharing/ShareBook.jsx
+++ bookshelf/src/components/Sharing/ShareBook.jsx
@@ -1,61 +1,50 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import { Box, Grid, GridItem } from "@chakra-ui/react";
 import {
-  Box,
-  Grid,
-  GridItem,
-  
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+  AlertDialogCloseButton,
+  useDisclosure,
+  Button,
+  Text,
+  Stack,
 } from "@chakra-ui/react";
 import Search from "../../components/Sharing/Search";
 import Filter from "../../components/Sharing/Filter";
 import ShareBookDetails from "../../components/Sharing/ShareBookDetails";
-import { Link } from 'react-router-dom'
 import axios from "axios";
 
 function ShareBook() {
-
   const [requestDetails, setRequestDetails] = useState({});
-
+  const [selectedRequest, setSelectedRequest] = useState(null); // State to track the selected request
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = React.useRef();
 
   useEffect(() => {
     const getRequestDetails = async () => {
       try {
-       
-        const response = await axios.get(`http://localhost:3000/api/v1/bookSharing/requests`);
+        const response = await axios.get(
+          `http://localhost:3000/api/v1/bookSharing/requests`
+        );
         console.log(response.data);
         setRequestDetails(response.data);
-       
       } catch (error) {
         console.log(error);
       }
     };
     getRequestDetails();
   }, []);
-  // const bookDetails = {
-  //   book1: {
-  //     id: '1',
-  //     title: "The Midnight Library",
-  //     author: "Matt Haig",
-  //     image: "https://images-na.ssl-images-amazon.com/images/I/81h2gWPTYJL.jpg",
-  //   },
-  //   book2: {
-  //     id: '2',
-  //     title: "The Vanishing Half",
-  //     author: "Brit Bennett",
-  //     image: "https://m.media-amazon.com/images/I/81ICvbFe2+L.jpg",
-  //   },
-  //   book3: {
-  //     id: '3',
-  //     title: "The Four Winds",
-  //     author: "Kristin Hannah",
-  //     image: "https://m.media-amazon.com/images/I/6132R6AHGjL.jpg",
-  //   },
-  //   book4: {
-  //     id: '4',
-  //     title: "The Sanatorium",
-  //     author: "Sarah Pearse",
-  //     image: "https://m.media-amazon.com/images/I/51k-rWw95NL.jpg",
-  //   },
-  // }
+
+  // Function to handle opening the AlertDialog and setting the selected request
+  const openAlertDialog = (requestId) => {
+    setSelectedRequest(requestDetails[requestId]);
+    onOpen();
+  };
+
   return (
     <Box
       border={"1px"}
@@ -76,29 +65,71 @@ function ShareBook() {
           <Filter />
         </GridItem>
 
-        <GridItem
-          rowSpan={8}
-          colSpan={5}
+        <GridItem rowSpan={8} colSpan={5}>
+          {Object.keys(requestDetails).map((item) => {
+            const createdAt = new Date(requestDetails[item].createdAt);
+            const formattedDate = createdAt.toISOString().split("T")[0]; // Extract and format the date
+            return (
+              <div key={item} onClick={() => openAlertDialog(item)}>
+                {/* Attach a click handler to open the AlertDialog */}
+                <ShareBookDetails
+                  bookName={requestDetails[item].bookName}
+                  userName={requestDetails[item].userName}
+                  details={requestDetails[item].details}
+                  createdAt={formattedDate} // Use the formatted date
+                  imageURL={requestDetails[item].image}
+                />
+              </div>
+            );
+          })}
+        </GridItem>
+
+        <AlertDialog
+          motionPreset="slideInBottom"
+          leastDestructiveRef={cancelRef}
+          onClose={onClose}
+          isOpen={isOpen}
+          isCentered
         >
-          {Object.keys(requestDetails).map((item) => (
-            
-            // <Link to={`/shareBook/${bookDetails[item].id}`}
-            //   key={bookDetails[item].id}>
-              <ShareBookDetails
-                bookName={requestDetails[item].bookName}
-                userName={requestDetails[item].userName}
-                details={requestDetails[item].details}
-                createdAt={requestDetails[item].createdAt}
-                
-                imageURL={requestDetails[item].image}
-              />
-          
-            // </Link>
-          ))}
-          
-          </GridItem>
+          <AlertDialogOverlay />
+
+          <AlertDialogContent>
+            <AlertDialogHeader></AlertDialogHeader>
+            <AlertDialogCloseButton onClick={onClose} />
+
+            <AlertDialogBody>
+              {/* Display the details of the selected request in a Stack */}
+              {selectedRequest && (
+                <Stack spacing={4}>
+                  <div>
+                    <img src={selectedRequest.image ? (selectedRequest.image) : ("http://localhost:3000/uploads/default.jpeg")} alt="" />
+                  </div>
+                  <div>
+                    <strong>BookName:</strong> {selectedRequest.bookName}
+                  </div>
+                  <div>
+                    <strong>UserName:</strong> {selectedRequest.userName}
+                  </div>
+                  
+                  <div>
+                    <strong>Details:</strong> {selectedRequest.details}
+                  </div>
+                </Stack>
+              )}
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                Cancel
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       </Grid>
     </Box>
   );
 }
+
 export default ShareBook;
+
+//this is the new change added to the git problem
