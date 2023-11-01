176ced5edf1d2b522e6e0f342fa4304929fef543
Tharindu Epasingha <2020cs050@stu.ucsc.cmb.ac.lk>
2023-11-01 10:39:33 +0530

    gg

--- bookshelf/src/utils/axiosInstance.js
+++ bookshelf/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000/api/v1",
+    baseURL: "https://bookshelf-backend-llgd.onrender.com/api/v1",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
