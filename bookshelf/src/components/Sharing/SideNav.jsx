a434041ed38cdd4753f900ac11bf22bf2877b000
Tharindu Epasingha <2020cs050@stu.ucsc.cmb.ac.lk>
2023-11-02 00:40:54 +0530

    gg

--- bookshelf/src/components/Sharing/SideNav.jsx
+++ bookshelf/src/components/Sharing/SideNav.jsx
@@ -95,7 +95,7 @@ export function Sidenav() {
                 fontWeight="600"
                 lineHeight="10"
             >
-                Book Sharing
+                Book Exchange
             </Text>
             <Flex flexDirection={'column'} gap={2} w={'80%'} mb={10} mt={5} >
 
